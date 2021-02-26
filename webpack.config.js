295dbfd5f122a8c5a870d5e1387613ad5bcd84b7
kif11 <kovalewskiy@gmail.com>
2021-02-26 11:45:42 -0800

    Dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -33,5 +33,15 @@ module.exports = {
   performance: {
     maxEntrypointSize: 2000000, // 2MB
     maxAssetSize: 4000000, // 4MB
-  }
+  },
+  devServer: {
+    contentBase: './dist',
+    // compress: true,
+    // disableHostCheck: true,
+    // hot: false,
+    // inline: false,
+    port: 3000,
+    host: '0.0.0.0',
+    https: true,
+  },
 };

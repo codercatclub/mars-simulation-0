7e9a7606a35ab9a60d7f3cc22838420e975ea10c
Sneha Belkhale <sbelkhale@yahoo.com>
2021-03-03 14:19:59 -0800

    can enter back intio gallery

--- src/components/SceneTransition.js
+++ src/components/SceneTransition.js
@@ -16,6 +16,7 @@ export default {
 
 
     const door = document.querySelector("#vectorFieldDoor")
+    const galleryDoor = document.querySelector("#galleryDoor")
 
     door.addEventListener('object3dset', (event) => {
         let doorMesh = event.target.object3D.getObjectByProperty('type', 'Mesh');
@@ -23,22 +24,49 @@ export default {
         doorMesh.getWorldPosition(this.doorWorldPos);
         this.doorWorldPos.y = 0
     });
+
+    galleryDoor.addEventListener('object3dset', (event) => {
+        let doorMesh = event.target.object3D.getObjectByProperty('type', 'Mesh');
+        this.galleryDoorPos = new THREE.Vector3();
+        doorMesh.getWorldPosition(this.galleryDoorPos);
+        this.galleryDoorPos.y = 0
+    });
   },
 
   tick: function (time, timeDelta) {
     //check if we are near the door 
-    if(this.doorWorldPos && !this.enteredDoor) {
-        this.camera.getWorldPosition(this.cameraWorldPos);
-        this.cameraWorldPos.y = 0
-        const dist = this.cameraWorldPos.distanceTo(this.doorWorldPos)
-        if(dist < 0.5 && (this.cameraWorldPos.z < this.doorWorldPos.z)) {
-            this.enteredDoor = true;
+    this.camera.getWorldPosition(this.cameraWorldPos);
+    this.cameraWorldPos.y = 0
+
+
+    //distance to the door 
+    if(!this.doorWorldPos) return;
+
+    let distToDoor =  this.cameraWorldPos.distanceTo(this.doorWorldPos);
+
+    if(!this.canTransition && distToDoor > 1) {
+        this.canTransition = true
+    }
+
+    if(!this.canTransition) return;
+
+    if(distToDoor < 0.5) {
+        if(this.enteredDoor) {
+            this.voidScene.setAttribute("visible", "false")
+            this.cityScene.setAttribute("visible", "true")
+            this.enteredDoor = false;
+            console.log("Sw")
+            this.el.sceneEl.object3D.background = new THREE.Color( 0xffffff );
+            this.el.sceneEl.object3D.fog.color = new THREE.Color( 0xffffff )
+        } else {
             this.voidScene.setAttribute("visible", "true")
             this.cityScene.setAttribute("visible", "false")
-            this.cameraRigEl.components["mover"].TeleportToZero();
+            this.enteredDoor = true;
+            console.log("Hi")
             this.el.sceneEl.object3D.background = new THREE.Color( 0x000000 );
-            this.el.sceneEl.object3D.fog.color = new THREE.Color( 0x000000 );
+            this.el.sceneEl.object3D.fog.color = new THREE.Color( 0x000000 ); 
         }
+        this.canTransition = false;
     }
   },
 };
